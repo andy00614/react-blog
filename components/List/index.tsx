c09fe2b6b67c6f3703258a0078aaee3907f3515d
andy00614 <33732843@qq.com>
2020-03-30 18:28:57 +0800

    feat:complete List Component

---
+++ components/List/index.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+import s from "./index.module.scss";
+import dayjs from "dayjs";
+import { useRouter } from "next/router";
+import { NextPage } from "next";
+
+interface Iprops {
+  list: Array<{
+    title: string;
+    time: number;
+    articleId: string;
+  }>;
+}
+const List: NextPage<Iprops> = (props) => {
+  const router = useRouter();
+  const { list } = props;
+
+  const turnArticlePage = (id: string) => {
+    router.push(`/article/${id}`);
+  };
+
+  return (
+    <div className={s.container}>
+      {list.map((item) => (
+        <div key={item.title} className={s.article}>
+          <div className={s.time}>{dayjs(item.time).format("YYYY/MM/DD")}</div>
+          <div
+            className={s.title}
+            onClick={() => turnArticlePage(item.articleId)}
+          >
+            {item.title}
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+List.getInitialProps = async (ctx) => {
+  const list = [
+    { title: "first", time: 1585563975245, articleId: "1585563975245" },
+    { title: "second", time: 1585564006842, articleId: "1585564006842" },
+  ];
+  return {
+    list,
+  };
+};
+export default List;
